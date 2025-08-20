876bb859ff3e1601a4a85e437226c91f47070ab0
SB Designs <hello@sbdesigns.spike.team>
2025-08-20 15:30:12 +0200

    Update sanity.config.ts

--- sanity.config.ts
+++ sanity.config.ts
@@ -21,7 +21,7 @@ console.log(projectId);
 
 export default defineConfig({
   name: "default",
-  title: "Stablo Template",
+  title: "InMotion",
   basePath: "/studio",
   projectId: projectId,
   dataset: dataset,
